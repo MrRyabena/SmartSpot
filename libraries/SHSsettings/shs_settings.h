987b31264c054eea5bd019def3bcac36ff56057d
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2024-10-13 15:31:43 +0300

    Added shs_settings

---
+++ libraries/SHSsettings/shs_settings.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Comment out after making changes:
+#error Settings file not set up by user
+
+namespace shs
+{
+	namespace settings
+	{
+		inline constexpr auto WIFI_SSID = "";
+		inline constexpr auto WIFI_PASSWORD = "";
+	}
+}
