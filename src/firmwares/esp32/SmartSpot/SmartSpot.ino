6af86288d9d4695c371084c8dd157014e2ce00c9
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-05-12 18:09:50 +0300

    Corrected and tested.

--- src/firmwares/esp32/SmartSpot/SmartSpot.ino
+++ src/firmwares/esp32/SmartSpot/SmartSpot.ino
@@ -1,6 +1,8 @@
 
 #include "settings.h"
 
+#include <memory>
+
 #include <shs_SmartSpot.h>
 #include <shs_SmartSpotConfig.h>
 
@@ -8,30 +10,41 @@
 #include <shs_lib_GRGB_API.h>
 
 #include <shs_ControlWiFi.h>
-// #include <shs_TcpSocket.h>
+#include <shs_TcpSocket.h>
+
+#define SHS_SF_DEBUG
+#define DEBUG
+#include <shs_debug.h>
 
 
 shs::SmartSpot spot(config);
 
-// shs::TcpSocket client(config.SERVER_IP, config.PORT, config.ID);
+// shs::TcpSocket client(config.SERVER_IP, config.PORT, config.ID.getModuleID(), nullptr, 25);
 
-// shs::GRGB_API grgb_api(*spot.getGRGB(), config.ID);
-// shs::DTP dtp(client, grgb_api, config.ID);
+// shs::GRGB_API grgb_api(*spot.getGRGB(), shs::t::shs_ID_t(config.ID).setComponentID(1));
+shs::DTP dtp(config.ID.getModuleID());
 
 
 void setup()
 {
-    shs::ControlWiFi::connectWiFi();
+    dinit();
+    shs::ControlWiFi::connectWiFiWait();
 
     spot.start();
 
+
+    dtp.attachAPI(std::move(std::make_unique<shs::GRGB_API>(*spot.getGRGB(), config.ID)));
+    dtp.attachBus(std::move(std::make_unique<shs::TcpSocket>(config.SERVER_IP, config.PORT, config.ID.getModuleID())));
+    dtp.start();
 }
 
 void loop()
 {
     spot.tick();
+    dtp.tick();
+   // client.tick();
 
-    // if (client.connected()) dtp.tick();
-    // else if (client.connect(config.SERVER_IP, config.PORT)) clinet.write(&config.THIS_ID - 1, 2);
+    //if (client.client.connected()) dtp.tick();
+  //  else if (client.reconnect()) client.sendPacket(shs::DTP_APIpackets::getInitialPacket(config.ID.getModuleID()));
 
 }
--- src/firmwares/esp32/SmartSpot/settings.h
+++ src/firmwares/esp32/SmartSpot/settings.h
@@ -3,10 +3,10 @@
 #include <shs_SmartSpotConfig.h>
 #include <shs_Sensor.h>
 #include <shs_lib_SensorTypes.h>
+#include <shs_ID.h>
 
-
-shs::SmartSpotConfig config {
-    .ID = 11,
+constexpr shs::SmartSpotConfig config {
+    .ID = shs::ID(11, 1, 0),
     .SERVER_IP = "192.168.1.10",
     .PORT = 10002,
     .TEMP_SENSOR_TYPE = shs::Sensor::Type::THERMISTOR,
