2a736c89bdb4f09bf9bf0a24570b682b99e698cf
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-05-21 21:53:52 +0300

    Added AutoOTA.

--- src/firmwares/esp32/SmartSpot/SmartSpot.ino
+++ src/firmwares/esp32/SmartSpot/SmartSpot.ino
@@ -1,10 +1,11 @@
-
 #include "settings.h"
 
 #include <memory>
 
 #include <shs_SmartSpot.h>
 #include <shs_SmartSpotConfig.h>
+#include <shs_SmartSpotConfigReader.h>
+#include <shs_SmartSpot_API.h>
 
 #include <shs_DTP.h>
 #include <shs_lib_GRGB_API.h>
@@ -16,35 +17,92 @@
 #define DEBUG
 #include <shs_debug.h>
 
+#include <AutoOTA.h>
 
-shs::SmartSpot spot(config);
 
-// shs::TcpSocket client(config.SERVER_IP, config.PORT, config.ID.getModuleID(), nullptr, 25);
+std::shared_ptr<shs::SmartSpot> spot;
 
-// shs::GRGB_API grgb_api(*spot.getGRGB(), shs::t::shs_ID_t(config.ID).setComponentID(1));
 shs::DTP dtp(config.ID.getModuleID());
+
+void checkUpdate();
 
 
 void setup()
 {
     dinit();
+
     shs::ControlWiFi::connectWiFiWait();
 
-    spot.start();
+    shs::SmartSpotConfigReader::initFS();
+    shs::SmartSpotConfigReader::getConfig(config);
+    
+    
+    spot = std::make_shared<shs::SmartSpot>(config);
+
+    analogReadResolution(10);
+
+    spot->start();
+
+    // doutln(analogRead(config.TEMP_SENSOR_PIN));
+
+    checkUpdate();
 
 
-    dtp.attachAPI(std::move(std::make_unique<shs::GRGB_API>(*spot.getGRGB(), config.ID)));
+    dtp.attachAPI(std::move(std::make_unique<shs::GRGB_API>(*spot->getGRGB(), config.ID)));
+    dtp.attachAPI(std::move(std::make_unique<shs::SmartSpot_API>(*spot, shs::ID(config.ID.getModuleID(), 2, 0))));
     dtp.attachBus(std::move(std::make_unique<shs::TcpSocket>(config.SERVER_IP, config.PORT, config.ID.getModuleID())));
     dtp.start();
 }
 
+
 void loop()
 {
-    spot.tick();
+    spot->tick();
     dtp.tick();
-   // client.tick();
-
-    //if (client.client.connected()) dtp.tick();
-  //  else if (client.reconnect()) client.sendPacket(shs::DTP_APIpackets::getInitialPacket(config.ID.getModuleID()));
-
 }
+
+
+void checkUpdate()
+{
+    AutoOTA ota(SMART_SPOT_ESP32_VERSION, "https://raw.githubusercontent.com/MrRyabena/SmartSpot/v2.x.x/project.json");
+
+    String ver, notes;
+    if (ota.checkUpdate(&ver, &notes))
+    {
+      doutln("has update!");
+      doutln(ver);
+
+      doutln(ota.version());
+      doutln(notes);
+      
+      auto& grgb = *(spot->getGRGB());
+      
+      grgb.setBrightness(80);
+      grgb.setColor(0xff0000);
+      grgb.tick();
+      delay(500);
+      grgb.setBrightness(0);
+      grgb.tick();
+      delay(500);
+      grgb.setBrightness(80);
+      grgb.setColor(0xff0000);
+      grgb.tick();
+      delay(500);
+      grgb.setBrightness(0);
+      grgb.tick();
+      
+      ota.updateNow();
+      doutln("updated!");
+    }
+
+    if (ota.hasError())
+    {
+      doutln((int)ota.getError());
+    }
+
+    doutln("no update");
+
+    
+}
+
+
