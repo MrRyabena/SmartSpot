5f312cc71b586d79e814864b383c7def0772241b
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2026-01-01 18:14:51 +0300

    Added color music, separated tasks (cores 0-1).

--- src/firmwares/esp32/SmartSpot/SmartSpot.ino
+++ src/firmwares/esp32/SmartSpot/SmartSpot.ino
@@ -7,6 +7,12 @@
 #include <shs_SmartSpotConfigReader.h>
 #include <shs_SmartSpot_API.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include <shs_lib_RGBmusic.h>
+#include <shs_lib_RGBmusic_API.h>
+
 #include <shs_DTP.h>
 #include <shs_lib_GRGB_API.h>
 
@@ -21,36 +27,52 @@
 
 
 std::shared_ptr<shs::SmartSpot> spot;
+std::shared_ptr<shs::RGBmusic> rgb_music;
 
 shs::DTP dtp(config.ID.getModuleID());
 
+TaskHandle_t handle_rgbMusicTick;
+
 void checkUpdate();
+void rgbMusicTick(void *);
 
 
 void setup()
 {
     dinit();
 
-    shs::ControlWiFi::connectWiFiWait();
+    doutln(shs::ControlWiFi::connectWiFiWait() ? "WiFi connected" : "WiFi connection faild");
 
     shs::SmartSpotConfigReader::initFS();
     shs::SmartSpotConfigReader::getConfig(config);
     
     
     spot = std::make_shared<shs::SmartSpot>(config);
+    rgb_music = std::make_shared<shs::RGBmusic>(*(spot->getGRGB()), RGB_MUSIC_PIN);
 
-    analogReadResolution(10);
+    //analogReadResolution(10);
 
     spot->start();
+    rgb_music->start();
+    // rgb_music->analyzer.setDt(5000);
+    //spot->getGRGB()->setBrightness(255);
+    //rgb_music->setEffect(shs::RGBmusic::Effects::SHIFT_151);
 
-    // doutln(analogRead(config.TEMP_SENSOR_PIN));
+    xTaskCreatePinnedToCore(rgbMusicTick, "rgbMusicTick", 2048, nullptr, 0, &handle_rgbMusicTick, 0);
 
-    checkUpdate();
+    doutln(analogRead(config.TEMP_SENSOR_PIN));
+    doutln(spot->getTemperature());
+
+    //checkUpdate();
 
 
     dtp.attachAPI(std::move(std::make_unique<shs::GRGB_API>(*spot->getGRGB(), config.ID)));
     dtp.attachAPI(std::move(std::make_unique<shs::SmartSpot_API>(*spot, shs::ID(config.ID.getModuleID(), 2, 0))));
-    dtp.attachBus(std::move(std::make_unique<shs::TcpSocket>(config.SERVER_IP, config.PORT, config.ID.getModuleID())));
+    dtp.attachAPI(std::move(std::make_unique<shs::RGBmusic_API>(*rgb_music, shs::ID(config.ID.getModuleID(), 3, 0))));
+    doutln(config.ID.getModuleID());
+    auto client = std::make_unique<shs::TcpSocket>(config.SERVER_IP, config.PORT, config.ID.getModuleID());
+    client->setReconnectingTimeout(UINT32_MAX);
+    dtp.attachBus(std::move(client));
     dtp.start();
 }
 
@@ -59,8 +81,18 @@ void loop()
 {
     spot->tick();
     dtp.tick();
+    // rgb_music->tick();
 }
 
+
+void rgbMusicTick(void *)
+{
+  while (true)
+  {
+      rgb_music->tick();
+      // vTaskDelay(pdMS_TO_TICKS(10)); 
+  }
+}
 
 void checkUpdate()
 {
